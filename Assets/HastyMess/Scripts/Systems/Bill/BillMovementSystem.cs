3a760f58a5679cfd483f67ec8a59d42ab8533582
Ivan Yermakov <iyermakov@outlook.com>
2022-06-23 23:45:11 +0700

    Fix Bill's dash

---
+++ Assets/HastyMess/Scripts/Systems/Bill/BillMovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using HastyMess.Scripts.Data;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+
+namespace HastyMess.Scripts.Systems
+{
+    [UpdateAfter(typeof(BillControllerSystem))]
+    public partial class BillMovementSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var deltaTime = Time.DeltaTime;
+
+            Entities.ForEach((ref PhysicsVelocity velocity, in BillComponent bill) =>
+            {
+                var newVelocity = velocity.Linear.xy;
+
+                // Don't update velocity if dashing
+                if (!math.any(newVelocity >= new float2(bill.Speed, bill.Speed)))
+                    newVelocity += bill.MoveDirection * bill.Speed * deltaTime;
+
+                velocity.Linear.xy = newVelocity;
+            }).Run();
+        }
+    }
+}
