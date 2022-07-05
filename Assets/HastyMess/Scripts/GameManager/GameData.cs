0fb50af0feafd10150a743ff6634a0bc731b3bfd
Ivan Yermakov <iyermakov@outlook.com>
2022-07-05 12:27:35 +0700

    Add game manager and update Unity

---
+++ Assets/HastyMess/Scripts/GameManager/GameData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace HastyMess.Scripts
+{
+    [CreateAssetMenu(fileName = "GameData", menuName = "Data/Game")]
+    public class GameData : ScriptableObject
+    {
+        public Texture2D defaultCursor;
+        public Texture2D hoverCursor;
+    }
+}
